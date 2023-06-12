bd836e5034237c8c3d7f216bc716636e0b3525d3
DoBac2004 <111523003+DoBac2004@users.noreply.github.com>
2023-06-12 15:19:16 +0700

    Add files via upload

---
+++ Slot10.b1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slot10.b1
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                ArrayList inv = new ArrayList();
+                // Add elements to the list
+
+                inv.Add(new Product("A", 5.9, 3));//Product p1 = new Product(,,);
+                inv.Add(new Product("B", 8.2, 2));
+                inv.Add(new Product("C", 3.5, 4));
+                inv.Add(new Product("D", 1.8, 8));
+                Console.WriteLine("Product list:");
+                foreach (Product i in inv)
+                {
+                    Console.WriteLine(" " + i);
+                }
+            }
+
+        }
+}
+
